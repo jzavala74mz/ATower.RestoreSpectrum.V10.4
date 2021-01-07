8b16ed168f3c38a842b2ef3f36ead3085da50f2e
root <root@atower.labrhel8>
2021-01-07 10:37:09 -0800

    Codificación de la Restauraciòn del Sistema

--- RestoreLineaComando.sh
+++ RestoreLineaComando.sh
@@ -3,7 +3,9 @@ git add RestoreLineaComando.sh
 git add RestoreSpectrum.v10.4.yml
 git commit -m "Codificación de la Restauraciòn del Sistema"
 
+
 echo "Ejecutar la restauración de Spectrum 10.4 mediante el PlayBook"
 ansible-playbook RestoreSpectrum.v10.4.yml -vvv --extra-vars "tipo_restaurar=NORMAL name_restaurar_file=bd000000000.SBDD"
 echo "listo !!"
 
+git push
