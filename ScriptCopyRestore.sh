bb7cc2210b6421572e56b16c755b1b984c107d70
root <root@atower.vmwarelab>
2021-01-10 15:00:07 -0600

    Se implementa el script para reañizar ña transferencia correspondiente

--- ScriptCopyRestore.sh
+++ ScriptCopyRestore.sh
@@ -1,5 +1,5 @@
-# spawn scp /path_from/file_name user_name_here@to_host_name:/path_to
-spawn scp root@192.168.112.174:/home/jzavala74mz/Uploads/SpectrumInstall_10.4_Linux.tar.gz /root/SpectrumInstall_10.4_Linux.tar.gz
+# Transferir el archivo de recuperación de spectrum desde el servidor de tower (repositorios principales)
+spawn scp root@192.168.112.174:/home/jzavala74mz/Uploads/db_20210110_1307.SSdb /root/db_20210110_1307.SSdb
 expect "password:"
 send P4ssw0rd\n;
 interact
